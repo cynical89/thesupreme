d9a166fae57b144d4a6953fd6930cda9863a0004
Anthony Michelizzi <cynical89@gmail.com>
2017-04-08 14:35:42 -0700

    Movement update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour {
     #region Fields 
 
     [SerializeField]
-    private CharacterController _cc;
+    private Rigidbody2D _rb2D;
 
     private readonly float _speed = 5.0f;
+    private readonly float _rotationSpeed = 6.0f;
+    private float yMove = 0f;
 
     #endregion
 
@@ -23,6 +25,8 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         HandleLocalMovement();
+        HandleRotation();
+        ConstrainVelocity();
 	}
 
     #endregion
@@ -36,21 +40,41 @@ public class PlayerMovement : MonoBehaviour {
             return;
         }
 
-        var xMove = Input.GetAxis("Horizontal");
-        var yMove = Input.GetAxis("Vertical");
+        yMove = Input.GetAxis("Vertical");
+        var prevInput = yMove;
+        var moveDirection = transform.up * yMove * _speed;
+        _rb2D.AddForce(moveDirection);
+    }
 
-        var moveDirection = new Vector2(xMove, yMove);
-        moveDirection *= _speed;
-        _cc.Move(moveDirection * Time.deltaTime);
+    private void HandleRotation() {
+        var xMove = Input.GetAxis("Horizontal");
+        var rotation = new Vector3(0,0,-xMove);
+        rotation *= _rotationSpeed;
+        transform.Rotate(rotation);
+    }
+
+    private void ConstrainVelocity() {
+        if (_rb2D.velocity.y > 0) {
+            _rb2D.velocity = _rb2D.velocity.y >= 1f ? new Vector2(_rb2D.velocity.x, 1f) : _rb2D.velocity;
+        } else {
+            _rb2D.velocity = _rb2D.velocity.y <= -1f ? new Vector2(_rb2D.velocity.x, -1f) : _rb2D.velocity;
+        }
+
+        if (_rb2D.velocity.x > 0) {
+            _rb2D.velocity = _rb2D.velocity.x >= 1f ? new Vector2(1f, _rb2D.velocity.y) : _rb2D.velocity;
+        } else {
+            _rb2D.velocity = _rb2D.velocity.x <= -1f ? new Vector2(-1f, _rb2D.velocity.y) : _rb2D.velocity;
+        }
+
     }
 
     private void Init()
     {
-        if (gameObject.GetComponent<CharacterController>() == null)
+        if (gameObject.GetComponent<Rigidbody2D>() == null)
         {
-            gameObject.AddComponent<CharacterController>();
+            gameObject.AddComponent<Rigidbody2D>();
         }
-        _cc = gameObject.GetComponent<CharacterController>();
+        _rb2D = gameObject.GetComponent<Rigidbody2D>();
 
         // temp init for local game. This state needs to be set elsewhere.
         GameStateManager.GameState = GameStateManager.States.LocalGame;
