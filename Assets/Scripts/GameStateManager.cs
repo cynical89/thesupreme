910d797172b83967c0f9658591ffc30ff5cf4e19
Anthony Michelizzi <cynical89@gmail.com>
2017-04-08 15:20:59 -0700

    stuff

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager {
+
+    public static States GameState;
+
+	public enum States
+    {
+        Menu,
+        LocalGame,
+        NetworkGame
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    #region Fields 
+
+    [SerializeField]
+    private CharacterController _cc;
+
+    private readonly float _speed = 5.0f;
+
+    #endregion
+
+    #region Monobehaviors
+
+    // Use this for initialization
+    void Start () {
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleLocalMovement();
+	}
+
+    #endregion
+
+    #region Methods
+
+    private void HandleLocalMovement()
+    {
+        if (GameStateManager.GameState != GameStateManager.States.LocalGame)
+        {
+            return;
+        }
+
+        var xMove = Input.GetAxis("Horizontal");
+        var yMove = Input.GetAxis("Vertical");
+
+        var moveDirection = new Vector2(xMove, yMove);
+        moveDirection *= _speed;
+        _cc.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Init()
+    {
+        if (gameObject.GetComponent<CharacterController>() == null)
+        {
+            gameObject.AddComponent<CharacterController>();
+        }
+        _cc = gameObject.GetComponent<CharacterController>();
+
+        // temp init for local game. This state needs to be set elsewhere.
+        GameStateManager.GameState = GameStateManager.States.LocalGame;
+    }
+
+    #endregion
+}
